cb106fcd96b0b2352e5b3c77fa8547cdeb089ea7
isim <ihcsim@gmail.com>
2013-11-28 21:19:54 -0800

    Add ntp class to demonstrate class syntax and the package-file-service pattern.

---
+++ src/classes/ntp.pp
@@ -0,0 +1,34 @@
+class ntp{
+
+ case $operatingsystem{
+  centos, redhat:{
+    $service_name = 'ntpd'
+    $conf_file = 'ntp.conf.el'
+  }
+
+  debian, ubuntu: {
+    $service_name = 'ntp'
+    $conf_file = 'ntp.conf.debian'
+  }
+ }
+
+ package {'ntp':
+   ensure => installed
+ }
+
+ file {'ntp.conf':
+   path    => '/etc/ntp.conf',
+   ensure  => file,
+   require => Package['ntp'],
+   source  => "/tmp/${conf_file}"
+ }
+
+ service {'ntp':
+   name      => $service_name,
+   ensure    => running,
+   enable    => true,
+   subscribe => File['ntp.conf']
+ }
+}
+
+include ntp
